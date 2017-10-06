f9a0af99d19dc82b441b65f39cf87dca597a197a
RyoBamboo <itakedaka@gmail.com>
2017-10-06 18:29:18 +0900

    Initialized Commit

---
+++ heimusubi-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  heimusubi-ios
+//
+//  Created by 竹之下遼 on 2017/10/06.
+//  Copyright © 2017年 竹之下遼. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
